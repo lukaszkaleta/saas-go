cb0fca230c3460cfa404bd0d0bdec3351a869ae6
lk <lk@handverksdata.no>
2025-08-27 21:16:58 +0200

    user filesystem

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := v0.1.23
+VERSION := v0.1.24
 tags:
 	git tag "database/pg/${VERSION}"
 	git tag "universal/${VERSION}"
--- filestore/integration/aws-s3/bucket.go
+++ filestore/integration/aws-s3/bucket.go
@@ -13,18 +13,23 @@ import (
 )
 
 type S3Bucket struct {
-	S3Client *s3.Client
+	s3Client *s3.Client
+	name     string
+}
+
+func NewS3Bucket(s3Client *s3.Client, name string) *S3Bucket {
+	return &S3Bucket{s3Client: s3Client, name: name}
 }
 
 // UploadFile reads from a file and puts the data into an object in a bucket.
-func (basics S3Bucket) UploadFile(ctx context.Context, bucketName string, objectKey string, pathToFile string) error {
+func (basics S3Bucket) UploadFile(ctx context.Context, objectKey string, pathToFile string) error {
 	file, err := os.Open(pathToFile)
 	if err != nil {
 		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", pathToFile, err)
 	} else {
 		defer file.Close()
-		_, err = basics.S3Client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket: aws.String(bucketName),
+		_, err = basics.s3Client.PutObject(ctx, &s3.PutObjectInput{
+			Bucket: aws.String(basics.name),
 			Key:    aws.String(objectKey),
 			Body:   file,
 		})
@@ -33,14 +38,14 @@ func (basics S3Bucket) UploadFile(ctx context.Context, bucketName string, object
 			if errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
 				log.Printf("Error while uploading object to %s. The object is too large.\n"+
 					"To upload objects larger than 5GB, use the S3 console (160GB max)\n"+
-					"or the multipart upload API (5TB max).", bucketName)
+					"or the multipart upload API (5TB max).", basics.name)
 			} else {
 				log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
-					pathToFile, bucketName, objectKey, err)
+					pathToFile, basics.name, objectKey, err)
 			}
 		} else {
-			err = s3.NewObjectExistsWaiter(basics.S3Client).Wait(
-				ctx, &s3.HeadObjectInput{Bucket: aws.String(bucketName), Key: aws.String(objectKey)}, time.Minute)
+			err = s3.NewObjectExistsWaiter(basics.s3Client).Wait(
+				ctx, &s3.HeadObjectInput{Bucket: aws.String(basics.name), Key: aws.String(objectKey)}, time.Minute)
 			if err != nil {
 				log.Printf("Failed attempt to wait for object %s to exist.\n", objectKey)
 			}
