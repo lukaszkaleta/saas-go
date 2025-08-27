b5abe309beb02d2fa06361ceaebe9104317cc2da
lk <lk@handverksdata.no>
2025-08-27 17:01:31 +0200

    no pointer reciver

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := v0.1.13
+VERSION := v0.1.14
 tags:
 	git tag "database/pg/${VERSION}"
 	git tag "universal/${VERSION}"
--- filestore/pg/pg-filestore.go
+++ filestore/pg/pg-filestore.go
@@ -6,10 +6,10 @@ type FilestoreSchema struct {
 	Db *pg.PgDb
 }
 
-func (filestoreSchema *FilestoreSchema) Create() error {
+func (filestoreSchema FilestoreSchema) Create() error {
 	return filestoreSchema.Db.ExecuteSql("ddl.sql")
 }
 
-func (filestoreSchema *FilestoreSchema) Drop() error {
+func (filestoreSchema FilestoreSchema) Drop() error {
 	return filestoreSchema.Db.ExecuteSql("drop.sql")
 }
