801184bf8e586f9dd8553056f2652dbda9d00b9d
lukaszkaleta <lukaszkaleta@gmail.com>
2025-10-27 18:29:47 +0100

    incremental version ...

--- universal/model-utils.go
+++ universal/model-utils.go
@@ -8,16 +8,16 @@ type HasId interface {
 	GetId() int64
 }
 
-func ModelsToMap(array []ModelAware) map[int64]ModelAware {
-	idMap := make(map[int64]ModelAware)
+func ModelsToMap[T ModelAware](array []T) map[int64]T {
+	idMap := make(map[int64]T)
 	for _, modelAware := range array {
 		idMap[modelAware.Model().GetId()] = modelAware
 	}
 	return idMap
 }
 
-func HasIdToMap(array []HasId) map[int64]HasId {
-	idMap := make(map[int64]HasId)
+func HasIdToMap[T HasId](array []T) map[int64]T {
+	idMap := make(map[int64]T)
 	for _, modelAware := range array {
 		idMap[modelAware.GetId()] = modelAware
 	}
