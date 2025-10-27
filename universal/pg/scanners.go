8dbe810cefb1ac2115245c0496c3ea3b25734aaa
lukaszkaleta <lukaszkaleta@gmail.com>
2025-10-27 16:29:24 +0100

    incremental version ...

--- universal/pg/scanners.go
+++ universal/pg/scanners.go
@@ -6,7 +6,7 @@ import (
 	"github.com/lukaszkaleta/saas-go/database/pg"
 )
 
-func ScanMany2many(db *pg.PgDb, query string, ids []*int64) (map[int64]int64, error) {
+func ScanMany2many(db *pg.PgDb, query string, ids []int64) (map[int64]int64, error) {
 	rows, err := db.Pool.Query(context.Background(), query, ids)
 	if err != nil {
 		return nil, err
