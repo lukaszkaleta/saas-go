a8a18cca4d70aea2e696f9262d40e032e027ddd5
lukaszkaleta <lukaszkaleta@gmail.com>
2026-02-01 14:45:04 +0100

    incremental version ...

--- category/categories-api.go
+++ category/categories-api.go
@@ -6,7 +6,7 @@ type Categories interface {
 	AddWithName(ctx context.Context, name string) (Category, error)
 	AllLocalized(ctx context.Context, country string, language string) ([]*CategoryModel, error)
 	ById(ctx context.Context, id int64) (Category, error)
-	ByIds(ctx context.Context, id []int64) ([]CategoryModel, error)
+	ByIds(ctx context.Context, id []int64) ([]*CategoryModel, error)
 }
 
 func Models(ctx context.Context, Categories []Category) []*CategoryModel {
--- category/go.mod
+++ category/go.mod
@@ -2,7 +2,7 @@ module github.com/lukaszkaleta/saas-go/category
 
 go 1.25.0
 
-require github.com/lukaszkaleta/saas-go/universal v0.2.157
+require github.com/lukaszkaleta/saas-go/universal v0.2.158
 
 replace github.com/lukaszkaleta/saas-go => ../
 
