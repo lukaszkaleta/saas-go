38c79df291c50c7f487d058c07a3169216898138
lukaszkaleta <lukaszkaleta@gmail.com>
2026-02-10 16:44:33 +0100

    incremental version ...

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := v0.2.207
+VERSION := v0.2.208
 tags:
 	git add .
 	git commit -m 'incremental version ...'
--- universal/ideable.go
+++ universal/ideable.go
@@ -6,6 +6,14 @@ type Idable interface {
 	ID() int64
 }
 
+type JustId struct {
+	id int64
+}
+
+func (ji *JustId) ID() int64 {
+	return ji.id
+}
+
 type Idables[T Idable] interface {
 	ById(ctx context.Context, id int64) (T, error)
 }
