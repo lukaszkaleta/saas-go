cb89fa03dd329d85907f6c1c0fd9507625ce1015
lukaszkaleta <lukaszkaleta@gmail.com>
2026-03-29 19:33:49 +0200

    incremental version ...

--- payment/integration/stripe/stripe-session-payments.go
+++ payment/integration/stripe/stripe-session-payments.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"log/slog"
+	"net/http"
 	"strconv"
 	"strings"
 
@@ -86,6 +87,11 @@ func (s *StripeSessionPayments) createStripeSession(ctx context.Context, interna
 
 		SuccessURL: stripe.String(s.successUrl),
 		CancelURL:  stripe.String(s.failureUrl),
+	}
+
+	//params.AddHeader("Vipps-Preview", "v1")
+	params.Params.Headers = http.Header{
+		"Vipps-Preview": []string{"v1"},
 	}
 
 	stripeSession, err := session.New(params)
